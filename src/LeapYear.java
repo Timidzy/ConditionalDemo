c093fbac86bffb12cd89c2798b998aaf2fe2ac2c
Timidzy <102879658+Timidzy@users.noreply.github.com>
2022-04-14 23:32:43 +0300

    Update LeapYear.java

--- src/LeapYear.java
+++ src/LeapYear.java
@@ -5,9 +5,14 @@ public class LeapYear {
         Scanner myScan = new Scanner(System.in);
         System.out.println("Hi!\nEnter a year");
         int year = myScan.nextInt();
-        if (year % 4 == 0){
+        if (year % 100 == 0 && year % 400 == 0 && year % 4 == 0) {
             System.out.println("This year is leap");
-        }else System.out.println("This year isn't leap");
-        System.out.println("See u next time)");
+        } else if (year % 400 != 0 && year % 100 == 0) {
+            System.out.println("This year isn't leap");
+        } else if (year % 4 == 0){
+            System.out.println("This year is leap");
+        }
+        System.out.println("Program was ended");
+
     }
 }
